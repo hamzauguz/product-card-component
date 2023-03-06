23a393f825b078c2692a0657bf83935fd0a822d8
hamzauguz <arastiranhamza@gmail.com>
2023-03-06 21:19:01 +0300

    make image card with responsive

--- src/cards/image-card/index.js
+++ src/cards/image-card/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import leftImg from "../../images/image-product-desktop.jpg";
+import leftMobileImg from "../../images/image-product-mobile.jpg";
+import { useMediaQuery } from "react-responsive";
 
 const ImageCard = () => {
+  const isMobileFloor = useMediaQuery({ query: "(max-width: 425px)" });
+
   return (
-    <div className="flex-1 bg-red-200">
-      <img src={leftImg} />
+    <div className=" flex-1 max-[700px]:flex-[0.65] bg-red-200 max-w-full relative">
+      <img
+        className="w-full absolute h-full object-cover "
+        src={isMobileFloor ? leftMobileImg : leftImg}
+      />
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,15 @@ module.exports = {
       colors: {
         "cream-background": "#f3eae3",
       },
+      screens: {
+        // => @media (min-width: 640px) { ... }
+
+        laptop: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        desktop: "1280px",
+        // => @media (min-width: 1280px) { ... }
+      },
     },
   },
   plugins: [],
