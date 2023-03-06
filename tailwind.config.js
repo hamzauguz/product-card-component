0af9dbe40aed40f31a6c1b0c3889902cb92ec140
hamzauguz <arastiranhamza@gmail.com>
2023-03-06 16:57:25 +0300

    make main-card

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      width: {
+        128: "32rem",
+      },
+      colors: {
+        "cream-background": "#f3eae3",
+      },
+    },
+  },
+  plugins: [],
+};
