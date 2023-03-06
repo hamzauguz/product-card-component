6b13d47144d0fe0d84e0342e9521354cdb8e3575
hamzauguz <arastiranhamza@gmail.com>
2023-03-06 17:00:26 +0300

    add rounded

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className=" bg-blue-200 w-full flex flex-row justify-between w-[700px] h-[500px]">
+    <div className=" bg-blue-200 w-full flex flex-row justify-between w-[700px] h-[500px] rounded-2xl overflow-hidden">
       <div className=" flex-1 bg-red-200">left</div>
       <div className="flex-1">right</div>
     </div>
