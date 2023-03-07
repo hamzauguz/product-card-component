5450c837f18cccdb5feca001bc35760e2c963d6d
hamzauguz <arastiranhamza@gmail.com>
2023-03-07 11:57:44 +0300

    add spans

--- src/cards/add-to-card/index.js
+++ src/cards/add-to-card/index.js
@@ -9,10 +9,16 @@ const AddToCard = () => {
     //   overflow-x: hidden;
     //   max-width: center;
     <div className="flex-1 flex">
-      <div className=" bg-red-200 w-full h-full p-8">
+      <div className="  w-full h-full p-8 flex flex-col justify-between">
         <span className=" font-title_card tracking-[0.2em] text-sm opacity-50">
           PERFUME
         </span>
+        <span>Gabrielle Essence Eau De Parfum</span>
+        <div>
+          <span>$149.99</span>
+          <span>$169.99</span>
+        </div>
+        <span>Add to Cart</span>
       </div>
     </div>
   );
